1625f23ac0938314549de84f8d50debe5cbfc22e
Raihan Ahmed <raihan.tusher@yahoo.com>
2022-11-30 16:43:30 +0600

    services urls

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', index, name="dashboard"),
+
+    #service url
     path('services/', all_services, name="all_services"),
+    path('services/add/', add_service, name="add_service"),
+    path('services/edit/<int:id>', edit_service, name="edit_services"),
 
 ]
